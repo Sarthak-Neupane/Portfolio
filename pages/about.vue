a52071d4e7418e3226527be1ae47aea0906968d5
Sarthak Neupane <sarthakrocks2004@gmail.com>
2023-03-22 10:58:36 +0545

    made the about page responsive on all devices

--- pages/about.vue
+++ pages/about.vue
@@ -5,7 +5,7 @@
         </div>
         <div class="flex-grow flex justify-center items-center sm:items-stretch xl:items-center w-full">
             <div
-                class="flex flex-col justify-center sm:justify-around items-start px-3 lg:px-16 2xl:px-32 pb-7 w-full gap-3 xl:flex xl:flex-row xl:justify-around xl:items-stretch xl:gap-5 xl:py-5 4xl:w-9/12 4xl:mx-auto">
+                class="flex flex-col justify-center sm:justify-around items-start px-3 lg:px-16 pb-7 w-full gap-3 xl:flex xl:flex-row xl:justify-around xl:items-stretch xl:gap-5 xl:py-5 4xl:w-11/12 4xl:mx-auto">
                 <Aboutme class="xl:w-full xl:flex xl:flex-row xl:justify-center xl:items-stretch" :play="play" />
                 <Workexp class="xl:w-full xl:flex xl:flex-row xl:justify-center xl:items-stretch" :play="play" />
             </div>
